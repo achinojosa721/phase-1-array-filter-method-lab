341a5a3a4bac50b9f55aa0e27d291e0711d08ee1
Alberto Hinojosa <ahinojosa721@gmail.com>
2022-07-17 15:58:38 -0500

    commit completed array filter lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, query) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === query.toLowerCase();
+    })
+};
+
+
+function fuzzyMatch(drivers, query){
+    const lengthOfQuery = query.length
+    return drivers.filter(function(driver){
+        return driver.slice(0, lengthOfQuery) === query
+    })
+}
+
+function matchName(drivers, query){
+    const result = drivers.filter(driver => driver.name === query)
+    return result;
+}
